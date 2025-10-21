0729762db53647ce599891f297e2b2a0ed2a1d88
Ashmit <sagarashmit@gmail.com>
2025-10-21 19:51:34 +0530

    add picture support to mkt app

--- mkt/models.py
+++ mkt/models.py
@@ -4,6 +4,8 @@ from django.conf import settings
 
 
 # Create your models here.
+# Version 1 Code
+"""
 class Ad(models.Model):
     title = models.CharField(
         max_length=200,
@@ -16,5 +18,28 @@ class Ad(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     # Shows up in admin list
+    def __str__(self):
+        return self.title
+"""
+
+
+# Version 2 Code
+class Ad(models.Model):
+    title = models.CharField(
+        max_length=200,
+        validators=[MinLengthValidator(2, "Title must be greater than 2 characters")],
+    )
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True)
+    text = models.TextField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    # Picture
+    picture = models.BinaryField(null=True, blank=True, editable=True)
+    content_type = models.CharField(
+        max_length=256, null=True, blank=True, help_text="The MIME type of the file"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.title
